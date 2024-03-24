3f6e61d3aa9cb521835e9cd213ee4f46f3e65d80
SuperZuo <296614333@qq.com>
2024-03-25 07:52:48 +0800

    23

---
+++ WPF/WPFBase/WPFBase/ViewModels/BMViewModel/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFBase.ViewModels.SMViewModel;
+
+namespace WPFBase.ViewModels.BMViewModel
+{
+    public class HomeViewModel : NavigationViewModel
+    {
+        public HomeViewModel(IContainerProvider containerProvider) : base(containerProvider)
+        {
+
+        }
+    }
+}
